4df63f175f2a879e89af3818b3c8f0caa0740969
BortnikovaOlga <49143816+BortnikovaOlga@users.noreply.github.com>
2019-06-09 22:33:46 +0700

    Домашняя работа 19.2

--- ru/bortnikova/task19/CheckApp.java
+++ ru/bortnikova/task19/CheckApp.java
@@ -1,6 +1,7 @@
 package ru.bortnikova.task19;
 
 import java.io.*;
+import java.math.BigDecimal;
 import java.util.Locale;
 import java.util.Scanner;
 
@@ -13,23 +14,26 @@ public class CheckApp {
 
         ) {
             scanner.useLocale(Locale.ENGLISH);
-            float total = 0;
-            prn.printf("%-20s %10s %10s %10s\n", "Наименование", "Цена", "Количество", "Стоимость");
-
+            BigDecimal total = new BigDecimal(0);
+            prn.printf("%-20s %10s %10s %10s\n\n", "Наименование", "Цена", "Количество", "Стоимость");
+            System.out.printf("%-20s %10s %10s %10s\n\n", "Наименование", "Цена", "Количество", "Стоимость");
             while (scanner.hasNext()) {
                 String nameProd = readNameProd(scanner);
-                boolean weight = (scanner.hasNextInt() ? false : true);
+                boolean weight = !scanner.hasNextInt();
                 float count = scanner.nextFloat();
                 float price = scanner.nextFloat();
-                float cost = (float) ((long) (count * price * 100)) / 100;
-                total += cost;
-                if (weight)
+                BigDecimal cost = new BigDecimal(count * price);
+                cost = cost.setScale(2, BigDecimal.ROUND_HALF_UP);
+                total = total.add(cost);
+                if (weight){
                     prn.printf("%-20s %10.2f %10.3f %10.2f\n", nameProd, price, count, cost);
-                else
+                    System.out.printf("%-20s %10.2f %10.3f %10.2f\n", nameProd, price, count, cost);}
+                else{
                     prn.printf("%-20s %10.2f %10d %10.2f\n", nameProd, price, (int) count, cost);
-
+                    System.out.printf("%-20s %10.2f %10d %10.2f\n", nameProd, price, (int) count, cost);}
             }
             prn.printf("\nИтого %47.2f", total);
+            System.out.printf("\nИтого %47.2f", total);
         } catch (FileNotFoundException ex) {
             ex.printStackTrace();
         } catch (IOException ex) {
@@ -40,7 +44,7 @@ public class CheckApp {
     // чтение составного наименования продукта
     static String readNameProd(Scanner scanner) {
         String nameProd = scanner.next();
-        if (!(scanner.hasNextInt() | scanner.hasNextFloat()))
+        if (!scanner.hasNextFloat())
             nameProd += " " + readNameProd(scanner);
         return nameProd;
     }
